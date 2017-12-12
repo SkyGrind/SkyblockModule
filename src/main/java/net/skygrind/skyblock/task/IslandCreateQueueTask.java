8e774f4b07d810daa61c15f40a791549cbed356a
Seth <dozingducklet@gmail.com>
2017-12-12 15:50:59 +0000

    Set home locationt to highest block at location

--- src/main/java/net/skygrind/skyblock/task/IslandCreateQueueTask.java
+++ src/main/java/net/skygrind/skyblock/task/IslandCreateQueueTask.java
@@ -100,6 +100,7 @@ public class IslandCreateQueueTask extends BukkitRunnable {
 
             if (item.getType() == IslandType.DEFAULT) {
                 center = center.clone().add(4, 8, 6);
+                center = center.getWorld().getHighestBlockAt(center).getLocation();
                 System.out.println("default island");
                 island.setSpawn(center);
             }
