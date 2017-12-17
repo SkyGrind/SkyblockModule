b147b962f4cf057f12fc5a66c66aab73e4dff9b8
Seth <dozingducklet@gmail.com>
2017-12-17 14:12:11 +0000

    Is warp now fix force argument

--- src/main/java/net/skygrind/skyblock/command/island/IslandWarpCommand.java
+++ src/main/java/net/skygrind/skyblock/command/island/IslandWarpCommand.java
@@ -21,44 +21,62 @@ public class IslandWarpCommand extends GooseCommand {
     @Override
     public void execute(Player sender, String[] args) {
         if (args.length != 1) {
+            if (args.length == 2) {
+                if (args[1].equalsIgnoreCase("--force")) {
+                    if (!sender.hasPermission("island.warp.force")) {
+                        sender.sendMessage(ChatColor.RED + "Usage: /island warp <Island>");
+                        return;
+                    }
+                    OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
+                    if (target == null) {
+                        sender.sendMessage(ChatColor.RED + String.format("No player with the name or UUID of '%s' was found.", args[0]));
+                        return;
+                    }
+                    doWarp(sender, target, true);
+                }
+                return;
+            }
             sender.sendMessage(ChatColor.RED + "Usage: /island warp <Island>");
             return;
         }
+
         OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
         if (target == null) {
             sender.sendMessage(ChatColor.RED + String.format("No player with the name or UUID of '%s' was found.", args[0]));
             return;
         }
+        doWarp(sender, target, false);
+    }
 
+    private void doWarp(final Player sender, final OfflinePlayer target, final boolean force) {
         Island island = SkyBlock.getPlugin().getIslandRegistry().findByUniqueId(target.getUniqueId());
         if (island == null) {
-            sender.sendMessage(ChatColor.RED + String.format("No Island with the Owner or Member of '%s' could be found.", args[0]));
+            sender.sendMessage(ChatColor.RED + String.format("No Island with the Owner or Member of '%s' could be found.", target.getName()));
             return;
         }
         if (island.getLocked()) {
-            sender.sendMessage(ChatColor.RED + String.format("The island '%s' is currently locked.", args[0]));
+            sender.sendMessage(ChatColor.RED + String.format("The island '%s' is currently locked.", target.getName()));
             return;
         }
-
-        if (island.getWarpLocation() == null) {
-            sender.sendMessage(ChatColor.RED + String.format("The Island '%s' doesn't not a warp location set.", args[0]));
-            return;
+        GooseLocation location = island.getWarpLocation();
+        if (location == null) {
+            if (force) {
+                location = GooseLocation.fromLocation(island.getSpawn());
+            } else {
+                sender.sendMessage(ChatColor.RED + String.format("The Island '%s' doesn't not a warp location set.", target.getName()));
+                return;
+            }
         }
-        sender.sendMessage(ChatColor.GREEN + String.format("You will be teleported to %s's island in 3 seconds.", args[0]));
+        sender.sendMessage(ChatColor.GREEN + String.format("You will be teleported to %s's island in 3 seconds.", target.getName()));
+        GooseLocation finalLocation = location;
         new BukkitRunnable() {
             @Override
             public void run() {
                 if (!sender.isOnline())
                     return;
 
-                GooseLocation gooseLocation = island.getWarpLocation();
-                if (gooseLocation == null) {
-                    sender.sendMessage(ChatColor.GREEN + "The Island '%s' no longer has a warp location set.");
-                    return;
-                }
-
-                sender.teleport(gooseLocation.toLocation());
-                sender.sendMessage(ChatColor.GREEN + String.format("You have been teleported to %s's island.", args[0]));
+                sender.teleport(finalLocation.toLocation());
+                sender.sendMessage(ChatColor.GREEN + String.format("You have been teleported to %s's island.", target.getName()));
             }
         }.runTaskLater(API.getPlugin(), 3 * 20L);
     }
