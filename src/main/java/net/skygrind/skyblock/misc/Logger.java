23022d1c573d2e8ba3af6787bf6a28b93a16c453
Venal <venaldev@gmail.com>
2018-01-05 23:39:11 -0500

    test pushing for webhook

--- src/main/java/net/skygrind/skyblock/misc/Logger.java
+++ src/main/java/net/skygrind/skyblock/misc/Logger.java
@@ -7,6 +7,8 @@ import org.bukkit.plugin.Plugin;
  * Created by Matt on 2017-02-10.
  */
 public class Logger {
+    
+    // Test push for webhook
 
     public static void info(String msg) {
         ((Plugin) API.getPlugin()).getLogger().info(msg);
