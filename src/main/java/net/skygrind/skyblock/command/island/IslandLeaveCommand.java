71e0588b3b51c342cd03e925dbf9d6b255315189
Seth <dozingducklet@gmail.com>
2018-01-06 18:05:00 +0000

    isEmpty() method

--- src/main/java/net/skygrind/skyblock/command/island/IslandLeaveCommand.java
+++ src/main/java/net/skygrind/skyblock/command/island/IslandLeaveCommand.java
@@ -44,7 +44,7 @@ public class IslandLeaveCommand extends GooseCommand {
         }
 
         if (island.getOwner().equals(player.getUniqueId())) {
-            if (island.getMembers().size() > 0) {
+            if (island.getMembers().isEmpty()) {
                 MessageUtil.sendGood(player, "Opening owner selection...");
                 openNewOwnerSelector(player, island);
                 return;
