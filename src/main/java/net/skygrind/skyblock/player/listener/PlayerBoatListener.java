dc08170c599132f61766e863dfbb5a5b63014d1c
Seth <dozingducklet@gmail.com>
2018-01-11 13:24:36 +0000

    Change boat speed to a multiplier instead

--- src/main/java/net/skygrind/skyblock/player/listener/PlayerBoatListener.java
+++ src/main/java/net/skygrind/skyblock/player/listener/PlayerBoatListener.java
@@ -1,7 +1,5 @@
 package net.skygrind.skyblock.player.listener;
 
-import com.islesmc.modules.api.config.ModuleConfig;
-import com.islesmc.modules.api.module.ModuleManager;
 import net.skygrind.skyblock.SkyBlock;
 import net.skygrind.skyblock.configuration.ServerType;
 import org.bukkit.entity.Boat;
@@ -10,6 +8,7 @@ import org.bukkit.entity.Vehicle;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.vehicle.VehicleMoveEvent;
+import org.bukkit.permissions.PermissionAttachmentInfo;
 
 public class PlayerBoatListener implements Listener {
     
@@ -26,9 +25,22 @@ public class PlayerBoatListener implements Listener {
         if (!(vehicle instanceof Boat)) return;
         
         Boat boat = (Boat) vehicle;
-        
+
+        double maxSpeed = boat.getMaxSpeed() * getSpeedMultiplier(player);
+
         if (player.hasPermission("isles.boatspeed")) {
-            boat.setMaxSpeed(200D);
+            boat.setMaxSpeed(maxSpeed);
         }
+    }
+
+
+    private double getSpeedMultiplier(final Player player) {
+        double multiplier = 1D;
+        for (PermissionAttachmentInfo info : player.getEffectivePermissions()) {
+            if (info.getPermission().startsWith("isles.boatspeed.")) {
+                multiplier = Double.parseDouble(info.getPermission().replace("isles.boatspeed.", ""));
+            }
+        }
+        return multiplier;
     }
 }
