fd68d2ed59e3de815015c23f92d6fa6605a7eb91
Seth McAllister <dozingducklet@gmail.com>
2018-01-15 19:17:56 +0000

    I hAvE fOuNd A lOgIc ErRoR - Venal

--- src/main/java/net/skygrind/skyblock/island/listeners/isles/CoordinateBookListener.java
+++ src/main/java/net/skygrind/skyblock/island/listeners/isles/CoordinateBookListener.java
@@ -32,7 +32,7 @@ public class CoordinateBookListener implements Listener {
         if (meta == null)
             return;
 
-        if (meta.getDisplayName() != null || !meta.getDisplayName().equalsIgnoreCase(COORD_BOOK_UNSET))
+        if (meta.getDisplayName() != null && !meta.getDisplayName().equalsIgnoreCase(COORD_BOOK_UNSET))
             return;
 
         Island island = SkyBlock.getPlugin().getIslandRegistry().getIslandAt(player.getLocation());
