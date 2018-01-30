b373189f0e2fe337ca4f55e48f416859d3674b87
Seth McAllister <dozingducklet@gmail.com>
2018-01-30 17:35:37 +0000

    Changed and fixed help command

--- src/main/java/net/skygrind/skyblock/command/HelpCommand.java
+++ src/main/java/net/skygrind/skyblock/command/HelpCommand.java
@@ -1,5 +1,6 @@
 package net.skygrind.skyblock.command;
 
+import net.skygrind.skyblock.SkyBlock;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
@@ -12,15 +13,14 @@ public class HelpCommand extends BukkitCommand {
     @Override
     public boolean execute(CommandSender sender, String s, String[] args) {
         sender.sendMessage(ChatColor.GRAY.toString() + ChatColor.STRIKETHROUGH + "-----------------------------------------------------");
-        sender.sendMessage(ChatColor.AQUA + " Island help: " + ChatColor.WHITE + "/is help");
-        sender.sendMessage(ChatColor.AQUA + " Shop: " + ChatColor.WHITE + "/shop or visit the Shop NPC");
-        sender.sendMessage(ChatColor.AQUA + " Auction House: " + ChatColor.WHITE + "/ah or visit the Auction House NPC");
-        sender.sendMessage(ChatColor.AQUA + " Custom Enchants: " + ChatColor.WHITE + "/ce or visit the enchanter NPC");
-        sender.sendMessage(ChatColor.AQUA + " Custom Enchants: " + ChatColor.WHITE + "/ce or visit the enchanter NPC");
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', SkyBlock.getPlugin().getServerConfig().getPrimaryColor()) + " Island help: " + ChatColor.WHITE + "/is help");
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', SkyBlock.getPlugin().getServerConfig().getPrimaryColor()) + " Shop: " + ChatColor.WHITE + "/shop or visit the Shop NPC");
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', SkyBlock.getPlugin().getServerConfig().getPrimaryColor()) + " Auction House: " + ChatColor.WHITE + "/ah or visit the Auction House NPC");
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', SkyBlock.getPlugin().getServerConfig().getPrimaryColor()) + " Custom Enchants: " + ChatColor.WHITE + "/ce or visit the enchanter NPC");
         sender.sendMessage(" ");
-        sender.sendMessage(ChatColor.AQUA + " Website: " + ChatColor.WHITE + "http://skyparadise-mc.com");
-        sender.sendMessage(ChatColor.AQUA + " Discord: " + ChatColor.WHITE + "https://discord.gg/q8GZhgR");
-        sender.sendMessage(ChatColor.AQUA + " Shop: " + ChatColor.WHITE + "shop.skyparadisemc.com");
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', SkyBlock.getPlugin().getServerConfig().getPrimaryColor()) + " Website: " + ChatColor.WHITE + "http://skyparadise-mc.com");
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', SkyBlock.getPlugin().getServerConfig().getPrimaryColor()) + " Discord: " + ChatColor.WHITE + "https://discord.gg/q8GZhgR");
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', SkyBlock.getPlugin().getServerConfig().getPrimaryColor()) + " Shop: " + ChatColor.WHITE + "shop.skyparadisemc.com");
         sender.sendMessage(ChatColor.GRAY.toString() + ChatColor.STRIKETHROUGH + "-----------------------------------------------------");
         return false;
     }
